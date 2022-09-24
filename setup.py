845f0dba9f247dfabd9524479e6edd701631915a
Owen Lynch <root@owenlynch.org>
2022-09-23 20:36:35 -0700

    added my dependencies to be precompiled

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
   install_requires=['jupyter-server-proxy @ git+http://github.com/fonsp/jupyter-server-proxy@3a58aa5005f942d0c208eab9a480f6ab171142ef'],
 )
 
-# because this is a demo of Pluto, we add some popular packages to the global package env and precompile
+# Let's see if precompiling will make things faster?
 import os
-os.system('julia -e "import Pkg; Pkg.add([\\"DataFrames\\", \\"CSV\\", \\"Plots\\"]); Pkg.precompile()"')
+os.system('julia -e "import Pkg; Pkg.add([\\"AlgebraicPetri\\", \\"DifferentialEquations\\", \\"Plots\\"]); Pkg.precompile()"')
